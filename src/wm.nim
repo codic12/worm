010db490e964923867283933429477889ee9ffd1
Your Name <you@example.com>
2022-03-02 18:12:50 -0800

    Revert "test"
    This reverts commit 93382310ae6b79cc3e2451151ca7e4af78b06c61.

--- src/wm.nim
+++ src/wm.nim
@@ -734,9 +734,8 @@ proc renderTop*(self: var Wm; client: var Client) =
       self.XPutImage(image, client.frame.maximize, gc)
     of fpMinimize:
       minimizeExists = true
+
       if not fileExists self.config.minimizePath:
-        echo "aw"
-        echo self.config.minimizePath
         continue
 
       discard self.dpy.XMapWindow client.frame.minimize
