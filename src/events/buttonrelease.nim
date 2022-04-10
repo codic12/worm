c80777b73b585c0a20f255937738368ee0949312
codic <codic@linuxmail.org>
2022-04-09 22:17:18 -0700

    Solve #58

--- src/events/buttonrelease.nim
+++ src/events/buttonrelease.nim
@@ -7,8 +7,9 @@ proc handleButtonRelease*(self: var Wm; ev: XButtonEvent): void =
   #if ev.subwindow == None or ev.window == self.root: return
   if self.motionInfo.isSome: discard self.dpy.XUngrabPointer CurrentTime
   self.motionInfo = none MotionInfo
-  let clientOpt = self.findClient do (client: Client) ->
-      bool: client.frame.window == ev.window
-  if clientOpt.isNone: return
-  let client = clientOpt.get[0]
-  self.renderTop client[]
+  # let clientOpt = self.findClient do (client: Client) ->
+  #     bool: client.frame.window == ev.window
+  # if clientOpt.isNone: return
+  # let client = clientOpt.get[0]
+  # self.renderTop client[]
+  for client in self.clients.mitems: self.renderTop client
