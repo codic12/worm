3a827535636e2014ebe410c2d42cd43f477d0279
Ashlynn Anderson <pea@pea.sh>
2022-04-10 17:21:35 -0400

    Remove tap grab
    Is there a reason why we would need to capture ButtonPress events on
    windows currently?

--- src/events/maprequest.nim
+++ src/events/maprequest.nim
@@ -162,7 +162,7 @@ proc handleMapRequest*(self: var Wm; ev: XMapRequestEvent): void =
     for win in [close, maximize, minimize]: discard self.dpy.XGrabButton(1, mask, win,
         true, ButtonPressMask or PointerMotionMask, GrabModeAsync, GrabModeAsync,
             None, None)
-  for mask in [uint32 0, Mod2Mask, LockMask,
+  for mask in [uint32 Mod2Mask, LockMask,
          Mod3Mask, Mod2Mask or LockMask,
         LockMask or Mod3Mask, Mod2Mask or Mod3Mask,
         Mod2Mask or LockMask or Mod3Mask]:
